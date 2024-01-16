cd58fa4c99fda3356890b6d930c5866706c79b42
Anya <zhangyazhen@zhangyahendeMBP.home>
2024-01-17 02:06:17 +0800

    行動版導覽列

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@ var tl = gsap.timeline({ defaults: { duration: 1, ease: "power1.out"} });
     tl.from(".shop-name", { x: -100 })
       .from(".hero-title", { x: -100 }, "<")
       .from(".intro-text", { x: -100 }, "<")
-      .from(".img-cta", { x: -100 }, "<")
+      .from(".cta", { x: -100 }, "<")
       .from(".reservestep",{y: 100}, "<");
 
       gsap.from(".about", {
         scrollTrigger: {
           trigger: ".about",
-          start: "top center",
+          start: "top bottom",
         },
         opacity: 0,
         y: 200,
@@ -20,6 +20,15 @@ var tl = gsap.timeline({ defaults: { duration: 1, ease: "power1.out"} });
         scrollTrigger: {
           trigger: ".products",
           start: "top center",
+        },
+        opacity: 0,
+        y: 200,
+      });
+
+      gsap.from(".cta-bottom", {
+        scrollTrigger: {
+          trigger: ".cta-bottom",
+          start: "top bottom",
         },
         opacity: 0,
         y: 200,
--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import '/src/style.css'
+
+const menu = document.querySelector('.hamburger');
+const offcanvas = document.querySelector('.offcanvas');
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    offcanvas.classList.toggle('offcanvas-active');
+});
